55bea77539ab6a175f3c5477eb4335462b756f6e
or7r <or777r@gmail.com>
2018-12-17 17:56:00 +0200

    Add files via upload

--- components/drivetrain.py
+++ components/drivetrain.py
@@ -1,9 +1,14 @@
 import wpilib.drive
-
+import ctre
+import ctre.trajectorypoint
+import csv
 
 class Drivetrain:
     SPD_STATE = 1
     STR_STATE = 2
+
+    MIN_POINTS_IN_TALON = 100
+    BTN_BUFFER_CNT_ID = 3
 
     drive: wpilib.drive.DifferentialDrive
     solenoid: wpilib.DoubleSolenoid
@@ -12,6 +17,32 @@ class Drivetrain:
     def __init__(self):
         self.x = self.y = 0
         self.speed_state = self.strength_state = self.overload_joystick = False
+        self.enable_motion_profile = self.is_motion_profile_enabled= False
+        #
+        # self.right = ctre.WPI_TalonSRX(2)
+        # self.left = ctre.WPI_TalonSRX(1)
+        #
+        self.left = self.drive.leftMotor
+        self.left.__class__ = ctre.WPI_TalonSRX
+        self.left = self.drive.rightMotor
+        self.right.__class__ = ctre.WPI_TalonSRX
+
+        self.right.changeMotionControlFramePeriod(5)
+        self.left.changeMotionControlFramePeriod(5)
+
+        #self.setPIDF(self.right)
+        #self.setPIDF(self.left)
+
+        #self.fillPoints("points_left_Talon.csv", self.left)
+        #self.fillPoints("points_right_Talon.csv", self.right)
+
+
+    @staticmethod
+    def setPIDF(talon):
+        talon.config_kF(1)
+        talon.config_kP(0)
+        talon.config_kI(0)
+        talon.config_kD(0)
 
     def set_speed_state(self):
         self.speed_state = True
@@ -29,9 +60,57 @@ class Drivetrain:
         self.move_x(x)
         self.move_y(y)
 
-    def execute(self):
-        self.drive.arcadeDrive(self.y, self.x)
+    def motion_profile(self):
+        self.enable_motion_profile = True
 
+
+    def reset(self):
+        self.right.clearMotionProfileTrajectories()
+        self.left.clearMotionProfileTrajectories()
+
+        self.stop_event.set()
+
+        self.left.pushMotionProfileTrajectory()
+
+    # @staticmethod
+    # def fillPoints(filename, talon):
+    #     talon.pushMotionProfileTrajectory(ctre.trajectorypoint.TrajectoryPoint(velocity=0, isLastPoint=False,
+    #                                                                                zeroPos=True, timeDur=10))
+    #     with open(filename, "r") as csv_file:
+    #         csv_reader = csv.Reader(csv_file)
+    #
+    #         for line in csv_reader:
+    #             talon.pushMotionProfileTrajectory(ctre.trajectorypoint.TrajectoryPoint(position=line[0],
+    #                                                                                        velocity=line[1],
+    #                                                                                        timeDur=line[2],
+    #                                                                                        isLastPoint=False,
+    #                                                                                        zeroPos=False,
+    #                                                                                        profileSlotSelect0=0,
+    #                                                                                        profileSlotSelect1=0,))
+    #
+    #     talon.pushMotionProfileTrajectory(ctre.trajectorypoint.TrajectoryPoint(velocity=0, isLastPoint=True))
+    #
+    #     for i in range(Drivetrain.MIN_POINTS_IN_TALON):
+    #         talon.processMotionProfileBuffer()
+
+    def execute(self):
+        if self.enable_motion_profile and not self.is_motion_profile_enabled:
+            self.is_motion_profile_enabled = True
+            self.left.controlMode(ctre.ControlMode.MotionProfile)
+            self.right.controlMode(ctre.ControlMode.MotionProfile)
+            for i in range(3):
+                self.left.processMotionProfileBuffer()
+                self.right.processMotionProfileBuffer()
+            return None
+
+        elif not self.enable_motion_profile:
+            self.is_motion_profile_enabled = False
+
+        self.enable_motion_profile = False
+
+        self.left.controlMode(ctre.ControlMode.PercentOutput)
+        self.right.controlMode(ctre.ControlMode.PercentOutput)
+        self.drive.arcadeDrive(self.y, self.x)
         if self.speed_state:
             self.solenoid.set(self.solenoid.Value(self.SPD_STATE))
         elif self.strength_state:
--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 import ctre
 import magicbot
@@ -10,15 +11,12 @@ from components.drivetrain import Drivetrain
 from components.elevator import Elevator
 from components.grippers import Grippers
 from components.handles import Handles
-from controllers.angle_controller import AngleController
-
 
 class MyRobot(magicbot.MagicRobot):
     drivetrain: Drivetrain
     elevator: Elevator
     handles: Handles
     grippers: Grippers
-    angle_ctrl: AngleController
 
     WHL_DIAMETER = 15.24
     WHL_CIRC = WHL_DIAMETER * math.pi
@@ -32,30 +30,51 @@ class MyRobot(magicbot.MagicRobot):
     DIST_TO_TICKS = WHL_CIRC / (ENCODER_RES * QUAD_MULTIPLIER *
                                 STRENGTH_GEAR_RATIO)
 
-    MAX_SPEED = 0.65
+    ABS_TOLERANCE = 3 / DIST_TO_TICKS
+
+    MAX_SPEED = 3.5    #0.65
     MIN_SPEED = -MAX_SPEED
+
+
+
+    TIME_OUT_MS = 10
+    PID_LOOP_IDX = 0
+    SLOT_IDX = 0
 
     def createObjects(self):
 
-        self.lr_motor = ctre.WPI_TalonSRX(1)
-        self.lf_motor = ctre.WPI_TalonSRX(2)
+        self.left_rear_motor = ctre.WPI_TalonSRX(2)
+        self.left_front_motor = ctre.WPI_TalonSRX(1)
 
-        self.rr_motor = ctre.WPI_TalonSRX(5)
-        self.rf_motor = ctre.WPI_TalonSRX(6)
+        self.right_rear_motor = ctre.WPI_TalonSRX(5)
+        self.right_front_motor = ctre.WPI_TalonSRX(6)
 
-        self.rf_motor.configSelectedFeedbackSensor(ctre.WPI_TalonSRX.
-                                                   FeedbackDevice.
-                                                   CTRE_MagEncoder_Relative)
-        self.talon.setSensorPhase(True)
-        self.setOutputRange(self.MIN_SPEED, self.MAX_SPEED)
+        self.left_rear_motor.follow(self.left_front_motor)
+        self.right_rear_motor.follow(self.right_front_motor)
 
-        self.setAbsoluteTolerance(self.ABS_TOLERANCE)
 
-        self.ABS_TOLERANCE = (3 / self.DIST_TO_TICKS)
+        # self.right_front_motor.\
+        #     configSelectedFeedbackSensor(feedbackDevice=ctre.WPI_TalonSRX.
+        #                                  FeedbackDevice.CTRE_MagEncoder_Relative,
+        #                                  pidIdx=self.PID_LOOP_IDX, timeoutMs=self.TIME_OUT_MS)
+        #
+        # self.left_front_motor.\
+        #     configSelectedFeedbackSensor(feedbackDevice=ctre.WPI_TalonSRX.
+        #                                  FeedbackDevice.CTRE_MagEncoder_Relative,
+        #                                  pidIdx=self.PID_LOOP_IDX, timeoutMs=self.TIME_OUT_MS)
+        #
+        # self.right_front_motor.setSensorPhase(True)
+        # self.left_front_motor.setSensorPhase(False)
 
-        self.left = wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor)
-        self.right = wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor)
+        #self.rf_motor.setOutputRange(self.MIN_SPEED, self.MAX_SPEED)
+        #self.lr_motor.setOutputRange(self.MIN_SPEED, self.MAX_SPEED)
 
+
+        #self.left = wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor)
+        #self.right = wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor)
+
+        self.left = wpilib.SpeedControllerGroup(self.left_front_motor)
+        self.right = wpilib.SpeedControllerGroup(self.right_front_motor)
         self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
 
         self.drivetrain_solenoid = wpilib.DoubleSolenoid(2, 3)
@@ -81,8 +100,25 @@ class MyRobot(magicbot.MagicRobot):
         self.button_7 = wpilib.buttons.JoystickButton(self.joystick, 7)
         self.button_10 = wpilib.buttons.JoystickButton(self.joystick, 10)
         self.button_11 = wpilib.buttons.JoystickButton(self.joystick, 11)
+        self.profile_btn = wpilib.buttons.JoystickButton(self.joystick, 9)
+
+    def teleopInit(self):
+        self.start_time = time.time()
+        self.counter = 0
 
     def teleopPeriodic(self):
+        # if self.counter < 100:
+        #     self.counter += 1
+        #     self.drivetrain.move_y(1)
+        # elif self.counter==100:
+        #     self.end_time = time.time()
+        #     print("rpm right =", self.right_front_motor.getAnalogIn() / (self.end_time - self.start_time))
+        #     print("rpm left =", self.left_front_motor.getAnalogIn() / (self.end_time - self.start_time))
+        #     raise("exepected exception")
+
+        return None
+
+
         if not self.drivetrain.overload_joystick:
             self.drivetrain.move(self.joystick.getX(), -self.joystick.getY())
 
@@ -102,6 +138,8 @@ class MyRobot(magicbot.MagicRobot):
             self.drivetrain.set_speed_state()
         elif self.button_11.get():
             self.drivetrain.set_strength_state()
+        if self.profile_btn.get():
+            self.drivetrain.motion_profile()
 
 
 if __name__ == '__main__':
